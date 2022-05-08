ffacfe17624713240a5402774050d90716de1d2e
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2022-05-08 18:50:57 -0400

    Byte loading and unloading of images

---
+++ loadFromBytes.py
@@ -0,0 +1,35 @@
+# This file converts all images in /imgSource to bye representations of all their data in /imgBits
+import os
+import numpy as np
+from PIL import Image
+
+in_directory = 'imgBits'
+out_directory = 'imgRev'
+
+for filename in os.listdir(in_directory):
+    if filename.endswith("") and not filename.endswith(".head"):
+        print(filename)
+
+        # Get the size of the image
+        size = []
+        with open(in_directory+"/"+filename+".head") as f:
+            size.append(int(f.readline()))
+            size.append(int(f.readline()))
+        
+        with open(in_directory+"/"+filename,"rb") as f:
+            rgb = []
+            for y in range(size[1]):
+                row = []
+                for x in range(size[0]):
+                    colours = [int.from_bytes(f.read(1), byteorder="big") for i in range(3)]
+                    row.append(colours)
+                rgb.append(row)
+
+        # VISUALISATION OF CONVERTED
+        # Convert the pixels into an array using numpy
+        array = np.array(rgb, dtype=np.uint8)
+
+        # Use PIL to create an image from the new array of pixels
+        new_image = Image.fromarray(array)
+        new_image.save(out_directory+"/"+filename+'rev.png')
+                
---
+++ loadToBytes.py
@@ -0,0 +1,38 @@
+# This file converts all images in /imgSource to bye representations of all their data in /imgBits
+import os
+from PIL import Image
+
+in_directory = 'imgSource'
+out_directory = 'imgBits'
+
+for filename in os.listdir(in_directory):
+    if filename.endswith(".jpg") or filename.endswith(".png"):
+        print(filename)
+        
+        # Create the outfile
+        outFile = open(out_directory+'/'+os.path.splitext(filename)[0], 'w')
+        outFile.close()
+        # Once made, open to write bytes
+        outFile = open(out_directory+'/'+os.path.splitext(filename)[0], 'wb')
+        
+        # Ingest image
+        im = Image.open(in_directory+"/"+filename)
+        rgb_im = im.convert('RGBA')
+        size = im.size
+
+        # Write all da bits
+        for y in range(size[1]):
+            for x in range(size[0]):
+                px = im.getpixel((x,y))
+                # Write R, G B
+                for v in px:
+                    d = bytearray(v.to_bytes(2, byteorder='big'))
+                    outFile.write(d[1:3])
+        outFile.close()
+
+        header = open(out_directory+'/'+os.path.splitext(filename)[0]+".head", 'w')
+        header.write(str(im.width)+"\n")
+        header.write(str(im.height))
+
+
+                
